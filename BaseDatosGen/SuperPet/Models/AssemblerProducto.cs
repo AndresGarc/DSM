6e85d7915634b8defbaee431714f453d95d44e4b
Pablo <pabloguijarro96@gmail.com>
2018-11-30 14:00:46 +0100

    Añadir assembler incompleto de producto

---
+++ BaseDatosGen/SuperPet/Models/AssemblerProducto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BaseDatosGenNHibernate.EN.BaseDatos;
+
+namespace MvcApplication1.Models
+{
+    public class AssemblerProducto
+    {
+        public Producto ConvertENToModelUI(ProductoEN en)
+        {
+            Producto prod = new Producto();
+            prod.id = en.Id;
+        //    prod.Descripcion = en.ProductoDescripcion; //no se si funciona asi o hay que elegir un idioma y eso
+            prod.Nombre = en.Nombre;
+            prod.Precio = en.Precio;
+            prod.Stock = en.Stock;
+            prod.Imagen = en.URLFoto;
+            prod.IdCategoria = en.Categoria.Id;
+            prod.NombreCategoria = en.Categoria.Nombre;
+            //prod.Subcategoria = en.Categoria.Subcategoria;
+            prod.Destacado = en.Destacado;
+            prod.Oferta = en.Oferta;
+            prod.ValoracionMedia = en.ValoracionMedia;
+            IList<ProductoDescripcionEN> lista = en.ProductoDescripcion;
+            foreach (ProductoDescripcionEN pro in lista)
+            {
+                if (pro.Idioma == BaseDatosGenNHibernate.Enumerated.BaseDatos.IdiomaEnum.Castellano)//en vez de poner uno a mano seria con la cookie y lo de la internacionalizacion para elegir idioma
+                    prod.Descripcion = pro.Texto;
+            }
+
+            //ver cuales del resto que aparecen en ProductoEN tengo que poner y como. valoracionmedia lo calcula una cp y no se si tengo que ponerlo
+            //aqui o no por lo de que al crear me sale para anyadir el valor y no tiene que meterlo el admin
+
+            /*
+            prod.IdSupercategoria=en.Categoria.Supercategoria.Id;
+            prod.NombreSupercategoria=en.Categoria.Supercategoria.Nombre;
+            */
+            return prod;
+
+
+        }
+        public IList<Producto> ConvertListENToModel (IList<ProductoEN> ens){
+            IList<Producto> arts = new List<Producto>();
+            foreach (ProductoEN en in ens)
+            {
+                arts.Add(ConvertENToModelUI(en));
+            }
+            return arts;
+        }
+        
+    }
+}
---
+++ BaseDatosGen/SuperPet/Models/Producto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Producto//No se que hacer con los comentarios si cogerlos como lista o no cogerlos aqui porque necesito todos no uno
+    {
+        [ScaffoldColumn(false)]
+        public int id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int IdCategoria { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string NombreCategoria { get; set; }
+        /* No se si va por supercategoria o por subcategoria
+        [ScaffoldColumn(false)]
+        public string Subcategoria { get; set; }
+        */
+        [Display(Prompt = "Nombre del producto", Description = "Nombre del producto", Name = "Nombre ")]
+        [Required(ErrorMessage = "Debe indicar un nombre para el producto")]
+        [StringLength(maximumLength: 200, ErrorMessage = "El nombre no puede tener más de 200 caracteres")]
+        public string Nombre { get; set; }
+
+        [Display(Prompt = "Precio del producto", Description = "Precio del producto", Name = "Precio ")]
+        [Required(ErrorMessage = "Debe indicar un precio para el producto")]
+        [DataType(DataType.Currency, ErrorMessage = "El precio debe ser un valor numérico")]
+        [Range(minimum: 0, maximum: 10000, ErrorMessage = "El precio debe ser mayor que cero y menor de 10000")]
+        public double Precio { get; set; }
+
+        [Display(Prompt = "Stock del producto", Description = "Stock del producto", Name = "Stock ")]
+        [Required(ErrorMessage = "Debe indicar un stock para el producto")]
+        [DataType(DataType.Currency, ErrorMessage = "El stock debe ser un valor numérico")]
+        [Range(minimum: 0, maximum: 10000, ErrorMessage = "El stock debe ser cero o mayor que cero")]
+        public int Stock { get; set; }
+
+        [Display(Prompt = "Descripción del producto", Description = "Descripción del producto", Name = "Descripción ")]
+        [Required(ErrorMessage = "Debe indicar una descripción para el producto")]
+        [StringLength(maximumLength: 200, ErrorMessage = "La descripción no puede tener más de 200 caracteres")]
+        public string Descripcion { get; set; }
+
+
+        [Display(Prompt = "Imagen del producto", Description = "Imagen del producto", Name = "Imagen ")]
+        [Required(ErrorMessage = "Debe indicar una imagen del producto")]
+        public string Imagen { get; set; }
+
+        [ScaffoldColumn(true)]//No se si asi o como los otros que quiero mostrar al crear
+        public bool Destacado { get; set; }
+
+        [Display(Prompt = "Oferta del producto", Description = "Oferta del producto", Name = "Oferta ")]
+        [Required(ErrorMessage = "Debe indicar una oferta para el producto, si no tiene oferta poner un 0")]
+        [DataType(DataType.Currency, ErrorMessage = "El precio debe ser un valor numérico")]
+        [Range(minimum: 0, maximum: 1, ErrorMessage = "El oferta debe ser mayor que cero y menor de 1 (num. decimal)")]
+        public double Oferta { get; set; }
+
+        [ScaffoldColumn(false)]
+        public double ValoracionMedia { get; set; }
+    }
+}
