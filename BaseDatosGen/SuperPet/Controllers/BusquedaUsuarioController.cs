cdb5a5c4f9b41e725e88825c02ea861944f12cd8
Monica <nika.gonz6996@gmail.com>
2018-12-30 19:40:02 +0100

    busqueda 2.0

--- BaseDatosGen/SuperPet/Controllers/BusquedaUsuarioController.cs
+++ BaseDatosGen/SuperPet/Controllers/BusquedaUsuarioController.cs
@@ -13,23 +13,62 @@ namespace SuperPet.Controllers
     public class BusquedaUsuarioController : BasicController
     {
         // GET: HomeUsuario
-        public ActionResult Index(string titulo, string precio)
+        public ActionResult Index(string titulo, string categoria, string min, string max)
         {
             IList<Producto> salida = new List<Producto>();
-                if (String.IsNullOrEmpty(titulo) == false)
+            if (String.IsNullOrEmpty(titulo) == false)
+            {
+                SessionInitialize();
+                salida = new AssemblerProducto().ConvertListENToModel(new ProductoCEN(new ProductoCAD(session)).GetProductosByNombre(titulo, 0, -1));
+                SessionClose();
+            }
+
+            if (String.IsNullOrEmpty(min) == false || String.IsNullOrEmpty(max) == false)
+            {
+                if (String.IsNullOrEmpty(min))
+                    min ="0";
+                if (String.IsNullOrEmpty(max))
+                    max = "99999999999";
+                if (salida.Count <= 0)
                 {
                     SessionInitialize();
-                    salida = new AssemblerProducto().ConvertListENToModel(new ProductoCEN(new ProductoCAD(session)).GetProductosByNombre(titulo, 0, -1));
+                    salida = new AssemblerProducto().ConvertListENToModel(new ProductoCEN(new ProductoCAD(session)).GetProductosByPrecios(Convert.ToDouble(min), Convert.ToDouble(max)));
                     SessionClose();
                 }
-                /*
-                if (form["precio"] != null)
+                else
+                {
+                    IList<Producto> aux = new List<Producto>();
+                    foreach (var item in salida)
+                    {
+                        if(item.Precio>= Convert.ToDouble(min) && item.Precio<= Convert.ToDouble(max))
+                        {
+                            aux.Add(item);
+                        }
+                    }
+                    salida = aux;
+                }
+            }
+            if (String.IsNullOrEmpty(categoria) == false && categoria!="0")
+            {
+                if (salida.Count <= 0)
                 {
                     SessionInitialize();
-                    IList<Producto> buscarPrecio = new AssemblerProducto().ConvertListENToModel(new ProductoCEN(new ProductoCAD(session)).GetProductosByPrecios(Convert.ToDouble(form["precio"]),Convert.ToDouble(form["precio"])));
+                    salida = new AssemblerProducto().ConvertListENToModel(new ProductoCEN(new ProductoCAD(session)).GetProductosByCategoriaId(Convert.ToInt32(categoria),0,-1));
                     SessionClose();
                 }
-                */
+                else
+                {
+                    IList<Producto> aux = new List<Producto>();
+                    foreach (var item in salida)
+                    {
+                        if(item.IdCategoria == Convert.ToInt32(categoria))
+                        {
+                            aux.Add(item);
+                        }
+                    }
+                    salida = aux;
+                }
+            }
 
             return View(salida);
         }
